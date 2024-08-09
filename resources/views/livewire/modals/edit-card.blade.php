934e48d62a5f8f26e07c72a99c1cb3915a823f14
Ryan Mooney <ryan@texrus.com>
2024-08-09 14:52:58 -0800

    add the ability to create and edit notes on the cards

--- resources/views/livewire/modals/edit-card.blade.php
+++ resources/views/livewire/modals/edit-card.blade.php
@@ -6,6 +6,12 @@
             <x-input-error :messages="$errors->get('editCardForm.title')" class="mt-1"/>
 
         </div>
+        <div>
+            <x-input-label for="notes" value="Notes" class="sr-only"/>
+            <x-textarea class="w-full" id="notes" wire:model="editCardForm.notes" rows="6"/>
+            <x-input-error :messages="$errors->get('editCardForm.notes')" class="mt-1"/>
+
+        </div>
         <div class="flex items-center justify-between">
             <x-primary-button>Save</x-primary-button>
 
